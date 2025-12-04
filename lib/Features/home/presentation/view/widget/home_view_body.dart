1f16f73510890a83d21a159eff5bd9a59672a418
Marwan_Tamer <125447155+Marwan4444@users.noreply.github.com>
2025-12-04 10:30:43 +0200

    best seller item part 2

--- lib/Features/home/presentation/view/widget/home_view_body.dart
+++ lib/Features/home/presentation/view/widget/home_view_body.dart
@@ -1,14 +1,14 @@
 import 'package:bookly/Features/home/presentation/view/widget/book_listview.dart';
 import 'package:bookly/Features/home/presentation/view/widget/custom_appbar.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +17,14 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        CustomAppBar(),
-       FeaturedBookListview(),
-       SizedBox(height: 50,),
-       Text("Best Seller",style: Styles.textStyle18,),
-       SizedBox(height: 20,),
-       BestSellerListViewItem(),
-       ],
-       ),
+          CustomAppBar(),
+          FeaturedBookListview(),
+          SizedBox(height: 50),
+          Text("Best Seller", style: Styles.textStyle18),
+          SizedBox(height: 20),
+          BestSellerListViewItem(),
+        ],
+      ),
     );
   }
 }
@@ -35,7 +35,7 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 12 ,bottom: 15),
+      padding: const EdgeInsets.only(left: 12, bottom: 15, right: 12),
       child: Row(
         children: [
           SizedBox(
@@ -53,10 +53,46 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 18, right: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    "Harry Potter and the chamber  ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold, 
+                      height: 1.3,
+                      fontFamily:kGtSectraFine
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+
+                const SizedBox(height: 5),
+                Text(
+                  "J.K Rowling",
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  "19.99 \$",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-
