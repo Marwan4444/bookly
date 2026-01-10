2e035c0358e2ced92a5c517aa46e99e28291eb95
Marwan_Tamer <125447155+Marwan4444@users.noreply.github.com>
2026-01-10 18:47:40 +0200

    create failurers file

--- lib/Features/home/data/models/repos/home_repo.dart
+++ lib/Features/home/data/models/repos/home_repo.dart
@@ -1,7 +1,8 @@
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/utils/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<String, List<BookModel>>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
 }
